d231fe5e4d55a0ef7e10932fcf050d3ddbf8525a
nam3042007-droid <nam3042007@gmail.com>
2026-05-24 17:24:39 +0700

    Create model_loader.py

---
+++ model_loader.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+def create_model():
+
+    model = tf.keras.Sequential([
+
+        tf.keras.layers.Input(shape=(28, 28, 1)),
+
+        tf.keras.layers.Conv2D(
+            32,
+            (3,3),
+            activation="relu"
+        ),
+
+        tf.keras.layers.MaxPooling2D((2,2)),
+
+        tf.keras.layers.Flatten(),
+
+        tf.keras.layers.Dense(
+            128,
+            activation="relu"
+        ),
+
+        tf.keras.layers.Dense(
+            10,
+            activation="softmax"
+        )
+
+    ])
+
+    return model
